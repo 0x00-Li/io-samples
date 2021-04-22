f2b00b8b7c3a0b7462b7e0e3a544544dd7ae0407
zero <lidm@ziroom.com>
2021-04-22 23:41:35 +0800

    'enter'

---
+++ src/main/java/fit/ome/io/Code01_OSFileIO.java
@@ -0,0 +1,16 @@
+package fit.ome.io;
+
+/**
+ * @version 0.0.1-SNAPSHOT
+ * @auther Zero
+ * @date 2021/4/22
+ **/
+public class Code01_OSFileIO {
+    static byte[] data = "1234567890\n".getBytes();
+    static String path = "/tmp/testfileio/out.txt";
+
+    public static void main(String[] args) {
+
+    }
+
+}
