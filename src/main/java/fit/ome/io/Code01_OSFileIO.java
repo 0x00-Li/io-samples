adfe068e104c36701f78bd2291c6721f2331c440
zero <lidm@ziroom.com>
2021-04-24 23:51:49 +0800

    basic io

--- src/main/java/fit/ome/io/Code01_OSFileIO.java
+++ src/main/java/fit/ome/io/Code01_OSFileIO.java
@@ -1,5 +1,7 @@
 package fit.ome.io;
 
+import java.io.*;
+
 /**
  * @version 0.0.1-SNAPSHOT
  * @auther Zero
@@ -12,5 +14,32 @@ public class Code01_OSFileIO {
     public static void main(String[] args) {
 
     }
+
+    // ==
+//    测试直接写入速度
+    // 会每次调用系统io进行文件刷盘
+    public static void testBasicFileIO() throws IOException, InterruptedException {
+        File f = new File(path);
+        FileOutputStream os = new FileOutputStream(f);
+        while (true) {
+            Thread.sleep(1000);
+            os.write(data);
+        }
+
+    }
+
+    // 测试buffer的文件IO
+    // jvm 8kB syscall write(8kBbyte[])
+    // 会根据buffer中的大小没8kB刷一次盘，减少系统调用
+    public static void testBufferFileIO() throws IOException, InterruptedException {
+        File f = new File(path);
+        BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(f));
+        while (true) {
+            Thread.sleep(1000);
+            os.write(data);
+        }
+    }
+
+    // 测试文件的NIO
 
 }
